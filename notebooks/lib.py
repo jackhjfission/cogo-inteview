9e9839f1a23078de7de70245d9630935330a21d7
jackhjfission <jackhjfission@gmail.com>
2022-06-11 14:20:40 +0100

    Initial data exploration and cleaning

--- notebooks/lib.py
+++ notebooks/lib.py
@@ -8,3 +8,6 @@ DATA_DIRECTORY = os.path.join('.','..','data')
 RAW_DATA_DIRECTORY = os.path.join(DATA_DIRECTORY, 'raw')
 DATA_DICTIONARY_PATH = os.path.join(RAW_DATA_DIRECTORY, 'Interview2_DataChallenge_DataDictionary.csv')
 USER_DATA_RAW_PATH = os.path.join(RAW_DATA_DIRECTORY, 'Interview2_DataChallenge.csv')
+
+INTERIM_DATA_DIRECTORY = os.path.join(DATA_DIRECTORY, 'interim')
+USER_DATA_PATH = os.path.join(INTERIM_DATA_DIRECTORY, 'Interview2_DataChallenge.pkl')
