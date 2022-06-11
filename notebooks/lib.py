995517b96b7cbc12d776eb7dd36c3a378fa930bb
jackhjfission <jackhjfission@gmail.com>
2022-06-11 14:19:39 +0100

    Raw path definitions

---
+++ notebooks/lib.py
@@ -0,0 +1,10 @@
+""" some definitions
+"""
+
+import os
+
+DATA_DIRECTORY = os.path.join('.','..','data')
+
+RAW_DATA_DIRECTORY = os.path.join(DATA_DIRECTORY, 'raw')
+DATA_DICTIONARY_PATH = os.path.join(RAW_DATA_DIRECTORY, 'Interview2_DataChallenge_DataDictionary.csv')
+USER_DATA_RAW_PATH = os.path.join(RAW_DATA_DIRECTORY, 'Interview2_DataChallenge.csv')
